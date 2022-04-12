a69d7d9bd068e1df1c6443d8657fecc88605d0a5
Ariane Brandão <ariane_mad@hotmail.com>
2022-04-12 16:58:53 -0300

    add primeiro teste unitario

---
+++ src/components/Header/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+import { ActiveLink } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+test("active link renders correctly", () => {
+  const { debug } = render(
+    <ActiveLink activeClassName="active" href="/">
+      <a>Home</a>
+    </ActiveLink>
+  );
+
+  debug();
+});
